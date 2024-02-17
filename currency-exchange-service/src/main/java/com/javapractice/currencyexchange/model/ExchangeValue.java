bcf8bb3e91ac3fa20f2bfb5550939322fcfe1e25
Kalashree Kalpana <kalashreekalpana@MacBook-Air.lan>
2024-02-17 17:29:03 -0600

    microservice exchange

---
+++ currency-exchange-service/src/main/java/com/javapractice/currencyexchange/model/ExchangeValue.java
@@ -0,0 +1,61 @@
+package com.javapractice.currencyexchange.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "exchange_value")
+public class ExchangeValue {
+
+	@Id
+	private Long id;
+	@Column(name="currency_from")
+	private String from;
+	@Column(name="currency_to")
+	private String to;
+	@Column(name = "conversion_multiple")
+	private BigDecimal conversionMultiple;
+	private int port;
+	
+	public ExchangeValue() {}
+	public ExchangeValue(Long id, String from, String to, BigDecimal conversionMultiple) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+	
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	
+	@Override
+	public String toString() {
+		return "ExchangeValue [id=" + id + ", from=" + from + ", to=" + to + ", conversionMultiple="
+				+ conversionMultiple + "]";
+	}
+	
+	
+	
+}
